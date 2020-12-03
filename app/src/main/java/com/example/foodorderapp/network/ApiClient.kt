025f6fd0de3391b387133fa39439b38bd65fee1a
Tamas <mj.tamas@protonmail.com>
2020-12-03 02:33:58 +0200

    implemeted REST api connection with retrofit

--- app/src/main/java/com/example/foodorderapp/network/ApiClient.kt
+++ app/src/main/java/com/example/foodorderapp/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-const val BASEURL = "https://opentable.herokuapp.com/api"
+const val BASEURL = "https://opentable.herokuapp.com/api/"
 class ApiClient {
     companion object{
         private var retrofit: Retrofit?=null
--- app/src/main/java/com/example/foodorderapp/network/ApiInterface.kt
+++ app/src/main/java/com/example/foodorderapp/network/ApiInterface.kt
@@ -1,4 +1,10 @@
 package com.example.foodorderapp.network
 
+import com.example.foodorderapp.home.data.restaurant.RestaurantModel
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiInterface {
+    @GET("restaurants")
+    fun fetchAllRestaurants(): Call<List<RestaurantModel>>
 }
