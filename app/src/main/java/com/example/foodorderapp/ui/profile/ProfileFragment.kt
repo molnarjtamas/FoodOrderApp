29a3fab559555c03adb2f66052d4f1299a1771ff
Tamas <mj.tamas@protonmail.com>
2020-12-13 06:26:27 +0200

    added Profile Screen

---
+++ app/src/main/java/com/example/foodorderapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.foodorderapp.ui.profile
+
+import androidx.fragment.app.Fragment
+import com.example.foodorderapp.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile){
+}
